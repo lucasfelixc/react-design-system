ef23efe443f0ab36d5a17492f62e1384d67fa8bd
Lucas Felix <lucasfelixdev@gmail.com>
2022-10-16 14:59:21 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      return config.base = '/react-design-system/'
+    }
+
+    return config;
   }
 }
