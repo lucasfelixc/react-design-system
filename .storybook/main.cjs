87ff74a984b6541ab6613f88cc1cc7e19a4d0480
Lucas Felix <lucasfelixdev@gmail.com>
2022-10-16 15:07:23 -0300

    sb: fix on config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      return config.base = '/react-design-system/'
+      config.base = '/react-design-system/'
     }
 
     return config;
